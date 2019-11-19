ae8030c6d34cb40c245c631558aad281ba0122e1
Irthir <romain.schlotter@gmail.com>
2019-11-19 10:54:40 +0100

    Typedef tPoint et tDroite

--- Programmer_En_C_RS_V1_0/main.c
+++ Programmer_En_C_RS_V1_0/main.c
@@ -4,187 +4,88 @@
 #define TAILLE 100
 
 //Les prototypes
-void FormalismeTableau();
-void FormalismePointeur();
-void Exercice1();
 
-void Exercice2();
-void MaxMinTableau(int nTailleTab,int nTab[],int *nMinTab,int *nMaxTab);
+typedef struct tPoint{ //Signature du type def.
+    int nX;
+    int nY;
+}tPoint; //Déclaration du nouveau type.
 
-void Exercice3();
-void RangeTableau(int nTailleTab,int nTab[]);
+typedef struct tDroite{
+    tPoint tabPoint[TAILLE];
+}tDroite;
 
-void Exercice4();
-void SommeMatrice(int nTaille,double nMatrice1[],double nMatrice2[],double nMatrice3[]);
+//Mutateurs.
+void setX(tPoint *pt,int nNewX);
+void setY(tPoint *pt,int nNewY);
+//Accesseurs.
+int getX(tPoint pt);
+int getY(tPoint pt);
 
+void creaDroite(tDroite *dt, tPoint pt,int nIndex);
 
-//Les exercices.
+//DéplacePoint
+tPoint deplacePoint(tPoint pnt,int nV, int nW);
+
 int main()
 {
-    //Exercice1();
+    tPoint newPoint={0,0};
+    tPoint neoPoint={0,0};
+    setX(&newPoint,5);
+    setY(&newPoint,8);
 
-    //Exercice2();
+    printf("x : %d\n",getX(newPoint));
+    printf("y : %d\n",getY(newPoint));
 
-    //Exercice3();
+    tDroite dt;
+    int i;
+    for (i=0;i<TAILLE;i++)
+    {
+        creaDroite(&dt,newPoint,i);
+    }
 
-    Exercice4();
+    neoPoint=deplacePoint(newPoint,2,3);
 
+    printf("x : %d\n",getX(neoPoint));
+    printf("y : %d\n",getY(neoPoint));
 
 	return 0;
 }
 
-void Exercice1()
+void setX(tPoint *pt,int nNewX)
 {
-    FormalismeTableau();
-    FormalismePointeur();
+    pt->nX=nNewX;
 }
 
-void FormalismeTableau()
+void setY(tPoint *pt,int nNewY)
 {
-	int nTabMax[10]={0};
-	for (int i = 0; i < 10; i++)
-	{
-		printf("Veuillez entrer la valeur numero : %d.\n",i+1);
-		scanf("%d",&nTabMax[i]);
-	}
-    int nI;
-    int nMax=nTabMax[0];
-    int nMin=nTabMax[0];
-    //formalisme pointeur
-    for (nI=1;nI<10;nI++)
-    {
-        if (nTabMax[nI]>nMax)
-        {
-            nMax=nTabMax[nI];
-        }
-        if (nTabMax[nI]<nMin)
-        {
-            nMin=nTabMax[nI];
-        }
-    }
-    printf("Minimum = %d Maximum = %d.\n",nMin,nMax);
+    pt->nY=nNewY;
 }
 
-void FormalismePointeur()
+int getX(tPoint pt)
 {
-	int nTabMax[10]={0};
-	for (int i = 0; i < 10; i++)
-	{
-		printf("Veuillez entrer la valeur numero : %d.\n",i+1);
-		scanf("%d",nTabMax+i);
-	}
-    int nI;
-    int nMax=*(nTabMax);
-    int nMin=*(nTabMax);
-    //formalisme pointeur
-    for (nI=1;nI<10;nI++)
-    {
-        if (*(nTabMax+nI)>nMax)
-        {
-            nMax=*(nTabMax+nI);
-        }
-        if (*(nTabMax+nI)<nMin)
-        {
-            nMax=*(nTabMax+nI);
-        }
-    }
-    printf("Minimum = %d Maximum = %d.\n",nMin,nMax);
+    return pt.nX;
 }
 
-void Exercice2()
+int getY(tPoint pt)
 {
-    int nTailleTableau=15;
-    int nTabMin[nTailleTableau];
-    int nMin=0;
-    int nMax=0;
-
-    MaxMinTableau(nTailleTableau,nTabMin,&nMin,&nMax);
-
-    printf("Minimum = %d Maximum = %d.\n",nMin,nMax);
+    return pt.nY;
 }
 
-void MaxMinTableau(int nTailleTab,int nTab[],int *nMinTab,int *nMaxTab)
+void creaDroite(tDroite *dt, tPoint pt,int nIndex)
 {
-    int nI=0;
-    for (nI=0;nI<nTailleTab;nI++)
-    {
-        nTab[nI]=nI;
-    }
-    *nMinTab=nTab[0];
-    *nMaxTab=nTab[0];
-    for (nI=1;nI<nTailleTab;nI++)
-    {
-        if (nTab[nI]>*nMaxTab)
-        {
-            *nMaxTab=nTab[nI];
-        }
+    dt->tabPoint[nIndex].nX=pt.nX+nIndex;//On passe par la référence de dt pour atteindre le point.
+    dt->tabPoint[nIndex].nY=pt.nY+nIndex;//le +nIndex c'est pour avoir un nX et nY différents.
 
-        if (nTab[nI]<*nMinTab)
-        {
-            *nMinTab=nTab[nI];
-        }
-    }
+    //dt->tabPoint[nIndex]=pt; //Autre possibilité.
 }
 
-void Exercice3()
+tPoint deplacePoint(tPoint pnt,int nV, int nW)
+//BUT : Déplacer un point.
+//ENTREE : Le point à déplacer, la valeur de déplacement en X puis celle en Y.
+//SORTIE : Le point déplacé.
 {
-    int nTailleTableau=15;
-    int nTabMin[nTailleTableau];
-
-    RangeTableau(nTailleTableau,nTabMin);
-
-    printf("Minimum = %d Maximum = %d.\n",nTabMin[0],nTabMin[nTailleTableau-1]);
-}
-
-void RangeTableau(int nTailleTab,int nTab[])
-{
-    int nI=0;
-    for (nI=0;nI<nTailleTab;nI++)
-    {
-        nTab[nI]=nI+50;
-    }
-    int nTemp=0;
-    int nJ=0;
-    for (nJ=1;nJ<nTailleTab;nJ++)
-    {
-        for (nI=0;nI<nTailleTab-1;nI++)
-        {
-            if (nTab[nI]>nTab[nI+1])
-            {
-                nTemp=nTab[nI];
-                nTab[nI]=nTab[nI+1];
-                nTab[nI+1]=nTemp;
-            }
-        }
-    }
-}
-
-void Exercice4()
-{
-    int nTaille=10;
-    int nX=0;
-    double nMatrice1[nTaille];
-    double nMatrice2[nTaille];
-    double nMatrice3[nTaille];
-    for (nX=0;nX<nTaille;nX++)
-    {
-        nMatrice1[nX]=nX*3.0;
-        nMatrice2[nX]=nX*5.0;
-        nMatrice3[nX]=0.0;
-    }
-    SommeMatrice(nTaille,nMatrice1,nMatrice2,nMatrice3);
-
-    for (nX=0;nX<nTaille;nX++)
-    {
-        printf("%4.4lf \n",nMatrice3[nX]);
-    }
-}
-
-void SommeMatrice(int nTaille,double nMatrice1[],double nMatrice2[],double nMatrice3[])
-{
-    int nY=0;
-    for (nY=0;nY<nTaille;nY++)
-    {
-        nMatrice3[nY]=nMatrice1[nY]+nMatrice2[nY];
-    }
+    //Si on n'a qu'un seul mécanisme d'assignation, ça va.
+    setX(&pnt,(nV+getX(pnt))); //pnt.nX+=nV;
+    setY(&pnt,(nW+getY(pnt))); //pnt.nY+=nW;
+    return pnt;
 }
