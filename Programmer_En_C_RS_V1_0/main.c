7f6ae8da65b3e289e1e4a17898d6db42fdbce8ab
Irthir <romain.schlotter@gmail.com>
2019-11-25 12:58:56 +0100

    suite du cours sur les Types et Enums

--- Programmer_En_C_RS_V1_0/main.c
+++ Programmer_En_C_RS_V1_0/main.c
@@ -2,100 +2,81 @@
 #include <stdlib.h>
 #include <math.h>
 #define TAILLE 100
+//Les Typedef
 
-//Les includes du sleep.
-#include <windows.h> // notice this! you need it! (windows)
-//Les prototypes
-/*
-typedef struct tPoint{ //Signature du type def.
+typedef struct tPoint{//Signature du typdef
+
     int nX;
     int nY;
-}tPoint; //Déclaration du nouveau type.
+
+}tPoint,*ptPoint;//Declaration du nouveau type
+
+
 
 typedef struct tDroite{
+
     tPoint tabPoint[TAILLE];
+
 }tDroite;
 
-//Mutateurs.
-void setX(tPoint *pt,int nNewX);
-void setY(tPoint *pt,int nNewY);
-//Accesseurs.
-int getX(tPoint pt);
-int getY(tPoint pt);
 
-void creaDroite(tDroite *dt, tPoint pt,int nIndex);
+//Les enumerations
+typedef  enum couleur{jaune,rouge,bleu,vert} couleur;
+//Commence a index 0
+// Si typedef  enum couleur{jaune=1,rouge=2,bleu,vert} couleur;
+//Alors index commence a 1
 
-//DéplacePoint
-tPoint deplacePoint(tPoint pnt,int nV, int nW);
-*/
+
+typedef struct tInfoDroite{
+
+    char cNomDroite[100];
+    tDroite dr;
+    couleur coul;
+
+}tInfoDroite;
+
+
+
+
 int main()
 {
+    //renvoie a la declaration de l'alias de la structure tPoint
+    ptPoint pPoint=NULL;//Cette declaration correspond à un pointeur.
+
+
+    //Creation d'une varialbe de type tPoint
+
+    tPoint pts={0,0}; //Initialisation des membres de la structure
+
+    pPoint=&pts;
+
+    printf("x:%d\n",pPoint->nX);
+    printf("y:%d\n",pPoint->nY);
+
+
     int i=0;
-    for (i=0;i<10;i++)
-    {
-        printf("\a");
-        Sleep(1000);
+    tDroite dr;
+
+    for(i=0;i<TAILLE;i++){
+
+        dr.tabPoint[i]=pts;
+
     }
 
-/*    tPoint newPoint={0,0};
-    tPoint neoPoint={0,0};
-    setX(&newPoint,5);
-    setY(&newPoint,8);
+    tInfoDroite iDroite={"segment",{0,0,1,2},jaune};//Peut generer un warning car nous n'initialisons pas les 100 valeurs.
+    //Initilisation d'un membre de type tInfoDroite
 
-    printf("x : %d\n",getX(newPoint));
-    printf("y : %d\n",getY(newPoint));
 
-    tDroite dt;
-    int i;
-    for (i=0;i<TAILLE;i++)
-    {
-        creaDroite(&dt,newPoint,i);
+    for(i=0;i<TAILLE;i++){
+
+        printf("Nom de la droite : %s", iDroite.cNomDroite);
+        printf("x: %d", iDroite.dr.tabPoint[i].nX);
+        printf("y: %d", iDroite.dr.tabPoint[i].nY);
+        printf("y: %d", iDroite.coul);//Cela donne id(int) de la couleur
+
     }
 
-    neoPoint=deplacePoint(newPoint,2,3);
 
-    printf("x : %d\n",getX(neoPoint));
-    printf("y : %d\n",getY(neoPoint));
-*/
-	return 0;
-}
-/*
-void setX(tPoint *pt,int nNewX)
-{
-    pt->nX=nNewX;
-}
 
-void setY(tPoint *pt,int nNewY)
-{
-    pt->nY=nNewY;
+   return 0;
 }
-
-int getX(tPoint pt)
-{
-    return pt.nX;
-}
-
-int getY(tPoint pt)
-{
-    return pt.nY;
-}
-
-void creaDroite(tDroite *dt, tPoint pt,int nIndex)
-{
-    dt->tabPoint[nIndex].nX=pt.nX+nIndex;//On passe par la référence de dt pour atteindre le point.
-    dt->tabPoint[nIndex].nY=pt.nY+nIndex;//le +nIndex c'est pour avoir un nX et nY différents.
-
-    //dt->tabPoint[nIndex]=pt; //Autre possibilité.
-}
-
-tPoint deplacePoint(tPoint pnt,int nV, int nW)
-//BUT : Déplacer un point.
-//ENTREE : Le point à déplacer, la valeur de déplacement en X puis celle en Y.
-//SORTIE : Le point déplacé.
-{
-    //Si on n'a qu'un seul mécanisme d'assignation, ça va.
-    setX(&pnt,(nV+getX(pnt))); //pnt.nX+=nV;
-    setY(&pnt,(nW+getY(pnt))); //pnt.nY+=nW;
-    return pnt;
-}
-*/
