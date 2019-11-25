3c09371c9285241e9137e228c760918a28b2a6ab
Irthir <romain.schlotter@gmail.com>
2019-11-25 14:49:53 +0100

    Exercices Types Enumes en C

--- Programmer_En_C_RS_V1_0/main.c
+++ Programmer_En_C_RS_V1_0/main.c
@@ -1,82 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define TAILLE 100
+#define TAILLE 5
 //Les Typedef
 
-typedef struct tPoint{//Signature du typdef
-
-    int nX;
-    int nY;
-
-}tPoint,*ptPoint;//Declaration du nouveau type
-
+typedef struct tpoint
+{   int num;
+    float x;
+    float y;
+} tpoint;
 
 
-typedef struct tDroite{
-
-    tPoint tabPoint[TAILLE];
-
-}tDroite;
-
-
-//Les enumerations
-typedef  enum couleur{jaune,rouge,bleu,vert} couleur;
-//Commence a index 0
-// Si typedef  enum couleur{jaune=1,rouge=2,bleu,vert} couleur;
-//Alors index commence a 1
-
-
-typedef struct tInfoDroite{
-
-    char cNomDroite[100];
-    tDroite dr;
-    couleur coul;
-
-}tInfoDroite;
-
-
+//LES PROTOTYPES
+void exercice1();
+void exercice2();
+void initTableauTPoint (tpoint tpointTab[],int nTailleTab);
+void scanTableauTPoint(tpoint tpointTab[],int nTailleTab);
+void afficheTableauTPoint(tpoint tpointTab[],int nTailleTab);
 
 
 int main()
 {
-    //renvoie a la declaration de l'alias de la structure tPoint
-    ptPoint pPoint=NULL;//Cette declaration correspond à un pointeur.
+    //exercice1();
+    exercice2();
 
+    return 0;
+}
 
-    //Creation d'une varialbe de type tPoint
-
-    tPoint pts={0,0}; //Initialisation des membres de la structure
-
-    pPoint=&pts;
-
-    printf("x:%d\n",pPoint->nX);
-    printf("y:%d\n",pPoint->nY);
-
-
-    int i=0;
-    tDroite dr;
-
-    for(i=0;i<TAILLE;i++){
-
-        dr.tabPoint[i]=pts;
-
+void exercice1()
+{
+    int nI;
+    tpoint ptTableau[TAILLE];
+    for (nI=0;nI<TAILLE;nI++)
+    {
+        ptTableau[nI].num=nI+1;
+        ptTableau[nI].x=0.0;
+        ptTableau[nI].y=0.0;
     }
-
-    tInfoDroite iDroite={"segment",{0,0,1,2},jaune};//Peut generer un warning car nous n'initialisons pas les 100 valeurs.
-    //Initilisation d'un membre de type tInfoDroite
-
-
-    for(i=0;i<TAILLE;i++){
-
-        printf("Nom de la droite : %s", iDroite.cNomDroite);
-        printf("x: %d", iDroite.dr.tabPoint[i].nX);
-        printf("y: %d", iDroite.dr.tabPoint[i].nY);
-        printf("y: %d", iDroite.coul);//Cela donne id(int) de la couleur
-
+    for (nI=0;nI<TAILLE;nI++)
+    {
+        printf("Veuillez rentrer la valeur en x du point numero %d.\n",nI+1);
+        scanf("%f",&ptTableau[nI].x);
+        fflush(stdin);
+        printf("Veuillez rentrer la valeur en x du point numero %d.\n",nI+1);
+        scanf("%f",&ptTableau[nI].y);
+        fflush(stdin);
     }
+    for (nI=0;nI<TAILLE;nI++)
+    {
+        printf("La valeur en x du point numero %d est %f.\n",nI+1,ptTableau[nI].x);
+        printf("La valeur en y du point numero %d est %f.\n",nI+1,ptTableau[nI].y);
+    }
+}
 
+void exercice2()
+{
+    tpoint ptTableau[TAILLE];
+    initTableauTPoint(ptTableau,TAILLE);
+    scanTableauTPoint(ptTableau,TAILLE);
+    afficheTableauTPoint(ptTableau,TAILLE);
+}
 
+void initTableauTPoint (tpoint tpointTab[],int nTailleTab)
+{
+    int nI;
+    for (nI=0;nI<nTailleTab;nI++)
+    {
+        tpointTab[nI].num=nI+1;
+        tpointTab[nI].x=0.0;
+        tpointTab[nI].y=0.0;
+    }
+}
 
-   return 0;
+void scanTableauTPoint(tpoint tpointTab[],int nTailleTab)
+{
+    int nI;
+    for (nI=0;nI<TAILLE;nI++)
+    {
+        printf("Veuillez rentrer la valeur en x du point numero %d.\n",nI+1);
+        scanf("%f",&tpointTab[nI].x);
+        fflush(stdin);
+        printf("Veuillez rentrer la valeur en x du point numero %d.\n",nI+1);
+        scanf("%f",&tpointTab[nI].y);
+        fflush(stdin);
+    }
+}
+
+void afficheTableauTPoint(tpoint tpointTab[],int nTailleTab)
+{
+    int nI;
+    for (nI=0;nI<TAILLE;nI++)
+    {
+        printf("La valeur en x du point numero %d est %f.\n",nI+1,tpointTab[nI].x);
+        printf("La valeur en y du point numero %d est %f.\n",nI+1,tpointTab[nI].y);
+    }
 }
