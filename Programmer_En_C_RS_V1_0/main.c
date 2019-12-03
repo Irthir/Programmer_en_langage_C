0c3f60c8e4729679a1c60b3966bc2bbc1c6554e3
Irthir <romain.schlotter@gmail.com>
2019-12-03 09:37:33 +0100

    Cours sur les lectures et écritures de fichiers.

--- Programmer_En_C_RS_V1_0/main.c
+++ Programmer_En_C_RS_V1_0/main.c
@@ -5,54 +5,58 @@
 
 int main ()
 {
-  FILE * pFile;
-  /* Mode texte */
-  char cSaisie[TAILLE];
-  char cAutre[TAILLE];
+    FILE * pFile;
+    /* Mode texte */
+    char cSaisie[TAILLE];
+    char cAutre[TAILLE];
 
 
-  //Saisie utilisateur
-  printf("Veuillez saisir un texte a inserer\n");
-  fgets(cSaisie,TAILLE,stdin);
-  //Ouverture du fichier
-  pFile = fopen("myfile.txt", "w");
+    //Saisie utilisateur
+    printf("Veuillez saisir un texte a inserer.\n");
+    fgets(cSaisie,TAILLE,stdin);
+    //Ouverture du fichier
+    pFile = fopen("myfile.txt", "w");
 
-  //Ecriture dans le fichier
-  if (pFile != NULL) {
-    fprintf(pFile,"%s\n",cSaisie);
-    fclose (pFile);
-  }
+    //Ecriture dans le fichier.
+    if (pFile != NULL)
+    {
+        fprintf(pFile,"%s\n",cSaisie);
+        fclose (pFile);
+    }
 
-  pFile = fopen("myfile.txt","r");
-  printf("Le fichier contient :\n");
+    pFile = fopen("myfile.txt","r");
+    printf("Le fichier contient :\n");
 
-  //Lecture du fichier
-  if (pFile != NULL) {
-    fgets(cAutre,TAILLE,pFile);
-    printf("%s",cAutre);
-    fclose (pFile);
-  }
+    //Lecture du fichier.
+    if (pFile != NULL)
+    {
+        fgets(cAutre,TAILLE,pFile);
+        printf("%s",cAutre);
+        fclose (pFile);
+    }
 
 
-  /*Mode binaire */
-  int nSaisie;
-  int nAutre;
+    /*Mode binaire.*/
+    int nSaisie;
+    int nAutre;
 
-  printf("Veuillez saisir un entier a inserer\n");
-  scanf("%d",&nSaisie);
-  //fgets(cSaisie,TAILLE,stdin);
-  pFile = fopen("myfile.bin","wb");
-  if (pFile != NULL ) {
-    fwrite(&nSaisie,sizeof(int),sizeof(nSaisie),pFile);
-    fclose(pFile);
-  }
-  printf("Le fichier contient :\n");
-  pFile = fopen("myfile.bin","rb");
-  if (pFile != NULL) {
-    fread(&nAutre,sizeof(int),sizeof(nSaisie),pFile);
-    printf("%d",nAutre);
-    fclose(pFile);
-  }
+    printf("Veuillez saisir un entier a inserer\n");
+    scanf("%d",&nSaisie);
+    //fgets(cSaisie,TAILLE,stdin);
+    pFile = fopen("myfile.bin","wb");
+    if (pFile != NULL )
+    {
+        fwrite(&nSaisie,sizeof(int),sizeof(nSaisie),pFile);
+        fclose(pFile);
+    }
+    printf("Le fichier contient :\n");
+    pFile = fopen("myfile.bin","rb");
+    if (pFile != NULL)
+    {
+        fread(&nAutre,sizeof(int),sizeof(nSaisie),pFile);
+        printf("%d",nAutre);
+        fclose(pFile);
+    }
 
-  return 0;
+    return 0;
 }
